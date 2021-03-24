58529667578d2eefa4588324e457d2f59f69e717
evgenshegai <evgeniishegai@yandex.ru>
2021-03-24 21:44:05 +1000

    Job c параметрами

---
+++ src/db/scripts/create_001.sql
@@ -0,0 +1,4 @@
+create table rabbit (
+                        id serial primary key NOT NULL,
+                        created_day DATE
+)
--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -4,6 +4,7 @@ import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.*;
 import java.util.Properties;
 import static org.quartz.JobBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
@@ -11,32 +12,74 @@ import static org.quartz.TriggerBuilder.*;
 
 public class AlertRabbit {
 
-    private  final Properties properties = new Properties();
+    private Connection connection;
 
     public static void main(String[] args) {
-        ClassLoader loader = AlertRabbit.class.getClassLoader();
+
         AlertRabbit rabbit = new AlertRabbit();
-        try (InputStream is = loader.getResourceAsStream("rabbit.properties")) {
-            rabbit.properties.load(is);
-            is.close();
+        Properties properties = rabbit.getProperties();
+        try (Connection connection = rabbit.init(properties)) {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
+            JobDataMap data = new JobDataMap();
+            data.put("connection", connection);
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
             SimpleScheduleBuilder times = simpleSchedule()
                     .withIntervalInSeconds(Integer.parseInt(
-                            rabbit.properties.getProperty("rabbit.interval")))
-                            .repeatForever();
-            Trigger trigger = newTrigger().startNow().withSchedule(times).build();
+                            properties.getProperty("rabbit.interval")))
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException | IOException e) {
-            e.printStackTrace();
+            Thread.sleep(10000);
+            scheduler.shutdown();
+        } catch (SchedulerException | SQLException | InterruptedException se) {
+            se.printStackTrace();
         }
     }
 
+    private Connection init(Properties properties) {
+        try  {
+            Class.forName(properties.getProperty("driver-class-name"));
+            connection = DriverManager.getConnection(
+                    properties.getProperty("url"),
+                    properties.getProperty("username"),
+                    properties.getProperty("password"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    private Properties getProperties() {
+        Properties pro = new Properties();
+        ClassLoader loader = AlertRabbit.class.getClassLoader();
+        try (InputStream is = loader.getResourceAsStream("rabbit.properties")) {
+            pro.load(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return pro;
+    }
+
     public static class Rabbit implements Job {
+
         @Override
-        public void execute(JobExecutionContext context) throws JobExecutionException {
-            System.out.println("Rabbit runs here and i read data from rabbit.properties ...");
+        public void execute(JobExecutionContext context) {
+            Connection connection = (Connection) context.getJobDetail().getJobDataMap()
+                    .get("connection");
+            try (PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO rabbit(created_day) VALUES (?);")) {
+                statement.setDate(1,
+                        new Date(System.currentTimeMillis()));
+                statement.executeUpdate();
+            } catch (SQLException s) {
+                s.printStackTrace();
+            }
         }
     }
 }
