c8f4bf09eb9038f6c6a42470af98097cabd15cf1
evgenshegai <evgeniishegai@yandex.ru>
2021-04-08 22:09:44 +1000

    Grabber поправил кодировку для корректного отображения в браузере

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -6,6 +6,7 @@ import ru.job4j.html.SqlRuParse;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Properties;
 import static org.quartz.JobBuilder.newJob;
@@ -83,8 +84,8 @@ public class Grabber implements Grab {
                     try (OutputStream out = socket.getOutputStream()) {
                         out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                         for (Post post : store.getAll()) {
-                            out.write(post.toString().getBytes());
-                            out.write(System.lineSeparator().getBytes());
+                            out.write(post.toString().getBytes(StandardCharsets.UTF_8));
+                            out.write(System.lineSeparator().getBytes(StandardCharsets.UTF_8));
                         }
                     } catch (IOException io) {
                         io.printStackTrace();
