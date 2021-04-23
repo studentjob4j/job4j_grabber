ca057168af4d32b1a56610bc48967a3241e52385
evgenshegai <evgeniishegai@yandex.ru>
2021-04-23 22:11:16 +1000

    Шаблонизатор

---
+++ src/main/java/ru/job4j/ood/template/GeneratorDemo.java
@@ -0,0 +1,17 @@
+package ru.job4j.ood.template;
+
+/**
+ * @author Evgenii Shegai
+ * @version 1.0
+ * @since 23.04.2021
+ */
+
+import java.util.Map;
+
+public class GeneratorDemo implements Generator {
+
+    @Override
+    public String produce(String template, Map<String, String> args) {
+        return null;
+    }
+}
---
+++ src/test/java/ru/job4j/ood/template/GeneratorDemoTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.ood.template;
+
+/**
+ * @author Evgenii Shegai
+ * @version 1.0
+ * @since 23.04.2021
+ */
+
+import org.junit.Test;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class GeneratorDemoTest {
+
+    private final String template = "I am a ${name}, Who are ${subject}?";
+
+    @Test
+    public void whenGetValueByKey() {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "John");
+        map.put("subject", "you");
+        String result = new GeneratorDemo().produce(template, map);
+        assertThat(result, is("I am a John, Who are you"));
+    }
+
+    @Test (expected = NoSuchElementException.class)
+    public void whenKeyNotExist() {
+        Map<String, String> map = new HashMap<>();
+        map.put("anykey", "John");
+        map.put("we", "you");
+        String result = new GeneratorDemo().produce(template, map);
+    }
+
+// when map have more then 2 keys and values  throw exception
+    @Test (expected = UnsupportedOperationException.class)
+    public void whenMapHaveMoreThenTwoKey() {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "John");
+        map.put("key", "value");
+        map.put("subject", "you");
+        String result = new GeneratorDemo().produce(template, map);
+    }
+}
