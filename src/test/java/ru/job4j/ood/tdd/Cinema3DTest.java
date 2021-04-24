5293563662e7674f43989ebf85c06b738c97068b
evgenshegai <evgeniishegai@yandex.ru>
2021-04-24 17:10:44 +1000

    Что такое TDD? добавил тесты

--- src/test/java/ru/job4j/ood/tdd/Cinema3DTest.java
+++ src/test/java/ru/job4j/ood/tdd/Cinema3DTest.java
@@ -41,4 +41,33 @@ public class Cinema3DTest {
         List<Session> result = cinema.find(session2 -> true);
         assertThat(result.get(0), is(session));
     }
+
+// when buy two tickets on one place then get exception
+    @Test (expected = UnsupportedOperationException.class)
+    public void whenBuyTwoTicketsOnOnePlace() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        Ticket ticket2 = cinema.buy(account, 1, 1, date);
+    }
+
+    @Test (expected = UnsupportedOperationException.class)
+    public void whenIncorrectDate() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2019, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+    }
+
+    @Test (expected = UnsupportedOperationException.class)
+    public void whenIncorrectPlace() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, -1, 1, date);
+    }
 }
